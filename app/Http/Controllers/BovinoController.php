53259441eb0bc4f33317ae54a783c9079cdae21c
lucas <erick33rick681@gmail.com>
2023-03-09 23:12:02 -0300

    adding file/ac

--- app/Http/Controllers/BovinoController.php
+++ app/Http/Controllers/BovinoController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class BovinoContller extends Controller
+class BovinoController extends Controller
 {
     /**
      * Display a listing of the resource.
