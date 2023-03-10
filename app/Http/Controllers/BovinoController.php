d47068c5c7b72136666727a919dd74ff851dd550
lucas <erick33rick681@gmail.com>
2023-03-09 23:20:16 -0300

    adicionando Controller

--- app/Http/Controllers/BovinoController.php
+++ app/Http/Controllers/BovinoController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class BovinoController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource. hey
      */
     public function index()
     {
